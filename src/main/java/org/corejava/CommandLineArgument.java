cd2e9c098fb9a7213bb7d9060398ec25d9978793
razzaksr <razzaksr@gmail.com>
2022-10-21 18:02:47 +0530

    Arithmetic operators

---
+++ src/main/java/org/corejava/CommandLineArgument.java
@@ -0,0 +1,12 @@
+package org.corejava;
+
+public class CommandLineArgument {
+    public static void main(String[] vasan) {
+        System.out.println(vasan.length);
+        String myName=vasan[0];
+        System.out.println("My name is "+myName);
+        int experience=Integer.parseInt(vasan[1]);
+        experience=experience+2;
+        System.out.println("My experience "+experience);
+    }
+}
